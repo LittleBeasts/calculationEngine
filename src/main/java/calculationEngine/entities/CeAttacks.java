04cae70f41b4f160357e7827e08c38522dd98e1f
andirh <57002449+andirh@users.noreply.github.com>
2021-05-12 21:59:57 +0200

    Added further attacks

--- src/main/java/calculationEngine/entities/CeAttacks.java
+++ src/main/java/calculationEngine/entities/CeAttacks.java
@@ -8,6 +8,10 @@ public enum CeAttacks {
     Charge(CeBeastTypes.Boss, 20, 5, 2, 5, "Charge Attack"),
     Whip(CeBeastTypes.Boss, 12, 8, 5, 7, "Water Whip"),
     Scratch(CeBeastTypes.PlayerStandard, 10, 10, 2, 20, "Scratch"),
+    Arcane(CeBeastTypes.Earth, 20, 7, 2, 5, "Arcane Blast"),
+    Sucker(CeBeastTypes.Earth, 12, 10, 8, 15,  "Sucker Punch"),
+    Acid(CeBeastTypes.Water, 10, 10, 5, 15, "Acid Downpour"),
+    FireBall(CeBeastTypes.Fire, 15, 10, 3, 15, "Fire Ball"),
     Bite(CeBeastTypes.PlayerStandard, 10, 10, 3, 20, "Bite"),
     Flee(CeBeastTypes.PlayerStandard, 0, 0, 0, 0, "Flee"),
     Catch(CeBeastTypes.PlayerStandard, 10, 10, 2, 3, "Catch");
